575b66001b197ac40924698700274bf3337cddf8
JuanJo <JuanJo@email.com>
2022-02-23 14:58:16 -0500

    add Estado_de_bomba::INIT --> DONE

--- bomb/bomb.ino
+++ bomb/bomb.ino
@@ -7,13 +7,53 @@
 #define ARM_BTN 33
 
 // Selecciona uno según tu display.
-//SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_128_32);
-SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_64_48); 
+SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_64_48);
 
-void setup() {
+void task() {
+  enum class Estado_de_bomba {INIT, CONFIGURACION, BOMBA_ACTIVA, BOMBA_DESACTIVA};
+  static Estado_de_bomba estadodebomba =  Estado_de_bomba::INIT;
+  static uint8_t bombCounter;
+
+  switch (estadodebomba) {
+    case Estado_de_bomba::INIT: {
+
+        Serial.begin(115200);
+        pinMode(UP_BTN, INPUT_PULLUP);
+        pinMode(DOWN_BTN, INPUT_PULLUP);
+        pinMode(ARM_BTN, INPUT_PULLUP);
+        pinMode(BOMB_OUT, OUTPUT);
+        pinMode(LED_COUNT, OUTPUT);
+
+        display.init();
+        display.setContrast(255);
+        display.clear();
+        display.setTextAlignment(TEXT_ALIGN_LEFT);
+        display.setFont(ArialMT_Plain_16);
+
+
+        bombCounter = 20;
+        display.clear();
+        display.drawString(10, 20, String(bombCounter));
+        display.display();
+
+        Serial.println("Estado_de_bomba::INIT");
+
+        estadodebomba =  Estado_de_bomba::CONFIGURACION;
+        break;
+      }
+
+    default: {
+        Serial.println("Error");
+        break;
+      }
+  }
 
 }
 
-void loop() {
+void setup() {
+  task();
+}
 
+void loop() {
+  task();
 }
